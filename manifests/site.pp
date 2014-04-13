ec49a331a1aca41d4c03f9de622739078c01fa53
Thomas Mohme <thomas.mohme@gmx.net>
2014-04-13 14:56:48 +0200

    replace ire by jdk, add go-server, go-agent

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,6 @@
-package {'java-1.7.0-openjdk':
+package {'java-1.7':
   ensure  => installed,
+  name => 'java-1.7.0-openjdk-devel'
 }
 
 package {'git':
@@ -12,3 +13,30 @@ package {'git':
 #}
 
 include jenkins
+
+
+package {'go-agent':
+  ensure => installed,
+  provider => rpm,
+  source => 'http://download01.thoughtworks.com/go/13.4.1/ga/go-agent-13.4.1-18342.noarch.rpm',
+}
+
+service {'go-agent':
+  ensure  => running,
+  enable  => true,
+  require => Package['go-agent'],
+}
+
+
+package {'go-server':
+  ensure => installed,
+  provider => rpm,
+  source => 'http://download01.thoughtworks.com/go/13.4.1/ga/go-server-13.4.1-18342.noarch.rpm',
+}
+
+service {'go-server':
+  ensure  => running,
+  enable  => true,
+  require => Package['go-server'],
+}
+
